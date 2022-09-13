e856741997c2f74b14b95c7b862a0c706dcc05e7
Blick <dima.8m8@gmail.com>
2022-09-13 18:38:51 +0300

    feat: setup project and add meetup component

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'First meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 323 231 ',
+  },
+  {
+    id: 'm2',
+    title: 'Second meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 111 11111 ',
+  },
+];
+
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+};
+
+export default HomePage;
