33b3b852ba82c088e158e0a8bc7810a65529cad7
Blick <dima.8m8@gmail.com>
2022-09-14 12:56:44 +0300

    feat: connect mongodb and send new meetups to database

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,21 @@
+import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 const NewMeetup = () => {
-  const addMeetupHandler = (enteredMeetupData) => {
-    console.log(enteredMeetupData);
+  const router = useRouter();
+
+  const addMeetupHandler = async (enteredMeetupData) => {
+    const respronse = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      header: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await respronse.json();
+    console.log(data);
+    router.push('/');
   };
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
